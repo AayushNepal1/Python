f5bb33b876662e3cc0b16e4ae565057aacd70518
AayushNepal1 <aayushnepal282@ggmail.com>
2022-03-30 18:40:02 +0545

    Python

---
+++ exercise/testing/employ.py
@@ -0,0 +1,26 @@
+from module.employee import Employee
+
+first = "First Name: "
+last = "Last Name: "
+salary = "Salary: "
+
+my_employee = Employee(first, last, salary)
+my_employee.get_data()
+
+print("Enter 'q' to quit any time you want.")
+while True:
+    first = input("\nFirst Name: ")
+    if first == 'q' or first == 'q'.upper():
+        break
+    last = input("\nLast Name: ")
+    if last == 'q' or last == 'q'.upper():
+        break
+    salary = int(input("\nSalary: "))
+    if salary == 'q' or salary == 'q'.upper():
+        break
+    my_employee.stored_data(first,last, salary)
+
+# Display Data
+print("The following are the records of an employee's: ")
+my_employee.display_record()
+my_employee.give_raise()
---
+++ exercise/testing/module/employee.py
@@ -0,0 +1,34 @@
+from curses.ascii import isdigit
+
+
+class Employee:
+    """Creating an Employee Class."""
+    
+    
+    def __init__(self, first, last, salary):
+        self.first = first
+        self.last = last
+        self.salary = salary
+        self.store_data = []
+        
+    def get_data(self):
+        """Displaying The Questions"""
+        print("Enter your First Name, Last Name and Anual Salary: ")
+
+    def give_raise(self, increase=5000):
+        """Increment the salary of the Employee."""
+        amount = self.salary
+        if amount.isdigit():
+            self.salary = self.salary + increase
+        
+    def stored_data(self, *data):
+        """Store the employee data."""
+        self.store_data.append(data)
+
+ 
+    def display_record(self):
+        for data in self.store_data:
+            for emp in data:
+                print(f"- {emp}")
+            print("------------------")
+        print()
